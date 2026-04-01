5e17c8f06fe03a6717588d739ba8ef6113611b29
Aravind Kekkarla <aravindkekkarla24@gmail.com>
2026-04-01 10:08:54 +0530

    Added insert data query

---
+++ CRUD_Operations/insert_into_table.sql
@@ -0,0 +1,42 @@
+INSERT INTO
+  exams(
+    Subject_id,
+    Subject_name,
+    Syllabus,
+    Date_of_exam,
+    Duration_of_exam,
+    results_published_datetime,
+    pass_percentage,
+    is_result_released
+  )
+VALUES
+  (
+    1,
+    "Mathametics",
+    "Trgnomentrt,statics,mensuration",
+    "2026-03-17",
+    "03:00:00",
+    "2026-04-17 12:00:00",
+    78.5,
+    0
+  ),
+  (
+    2,
+    "Physics",
+    "light,sound,elecricity",
+    "2026-03-18",
+    "03:00:00",
+    "2026-04-18 12:00:00",
+    87.42,
+    0
+  ),
+  (
+    3,
+    "Social",
+    "constitution,economics,democracy",
+    "2026-03-18",
+    "03:00:00",
+    "2026-04-19 12:00:00",
+    91.07,
+    0
+  )
