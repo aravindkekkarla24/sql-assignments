adcd5b92e2f7d99806919b5310c055dc33d344d3
Aravind Kekkarla <aravindkekkarla24@gmail.com>
2026-04-01 09:48:43 +0530

    Added first SQL assignment - create table

---
+++ CRUD_Operations/create_table.sql
@@ -0,0 +1,12 @@
+create table exams(
+subject_id int,
+subject_name varchar(250),
+syllabus text,
+date_of_exam date,
+duration_of_exam time,
+results_published_datetime datetime,
+pass_percentage float,
+is_result_released boolean
+
+
+)
